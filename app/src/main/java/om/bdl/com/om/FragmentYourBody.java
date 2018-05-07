a7d8286df8ef03c419c642ce775d99d117c86783
Sinduja Prakash <pk.sinduja@gmail.com>
2018-05-07 13:34:41 +0200

    Initial Commit

---
+++ app/src/main/java/om/bdl/com/om/FragmentYourBody.java
@@ -0,0 +1,19 @@
+package om.bdl.com.om;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentYourBody extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.fragment_yourbody, null);
+    }
+}
---
+++ app/src/main/java/om/bdl/com/om/MainActivity.java
@@ -0,0 +1,71 @@
+package om.bdl.com.om;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_chat:
+                    fragment = new FragmentChat();
+                    break;
+
+                case R.id.navigation_dashboard:
+                    fragment = new FragmentDashboard();
+                    break;
+
+                case R.id.navigation_yourbody:
+                    fragment = new FragmentYourBody();
+                    break;
+
+                case R.id.navigation_breathe:
+                    fragment = new FragmentBreathe();
+                    break;
+
+                case  R.id.navigation_community:
+                    fragment = new FragmentCommunity();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new FragmentDashboard());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_main, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
