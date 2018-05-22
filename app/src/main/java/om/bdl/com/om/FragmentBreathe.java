cb93713f280947919cbd8b3fa336d20d391185fe
Sinduja Prakash <pk.sinduja@gmail.com>
2018-05-22 16:23:37 +0200

    Basic changes

--- app/src/main/java/om/bdl/com/om/FragmentBreathe.java
+++ app/src/main/java/om/bdl/com/om/FragmentBreathe.java
@@ -7,10 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FragmentBreathe extends Fragment {
+
+    private Timer timer;
+    private TimerTask timerTask;
+    TextView inhale;
+    int count = 0;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //just change the fragment_dashboard
@@ -25,5 +34,25 @@ public class FragmentBreathe extends Fragment {
         Glide.with(this)
                 .load(R.drawable.breathegif)
                 .into(imageView);
+         inhale = (TextView) getView().findViewById(R.id.textView3);
+         startTimer();
+    }
+
+    public void startTimer() {
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //Download file here and refresh
+                count++;
+                if (count % 2 == 0) {
+                    inhale.setText("now Inhale..");
+                } else {
+                    inhale.setText("and Exhale..");
+                }
+
+                timer.schedule(timerTask,0);
+            }
+        };
     }
 }
